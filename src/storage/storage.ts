7e7a2cc0f8ab14295bd270958c08aeb4ee0f65d7
AnotherPotato <joansebastian35@gmail.com>
2019-06-04 02:45:44 -0500

    Database added

---
+++ src/storage/storage.ts
@@ -0,0 +1,14 @@
+import { observable, action, toJS, values } from 'mobx';
+import * as papaparse from 'papaparse';
+import * as mathjs from 'mathjs'
+
+class Storage {
+
+
+    constructor () {
+        
+    }
+}
+
+const storage = new Storage();
+export default storage;
